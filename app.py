567718e8f5723c481184d8423c311f9c305b3a93
Jonnu7 <andelinjohannes@gmail.com>
2022-03-10 13:46:10 +0200

    index.html and app.py updated. Requirements added

--- app.py
+++ app.py
@@ -5,17 +5,13 @@ from datacall import main
 app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
-def index():    
-        
-    to_send_obj=main()
-    print(to_send_obj)
-                 
+def index():        
+    to_send_obj=main()                
     return render_template('index.html', to_send=to_send_obj)
 
 @app.route('/favicon.ico') 
 def favicon(): return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-index()
 
 if __name__ == "__main__":
         app.run(debug=True)
